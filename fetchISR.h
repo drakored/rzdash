40d6af5ac3b7d99d75b10bb602dd96e1cbefd588
mobyfab <mobyfab@9e2985f0-feec-11de-89cc-8daf83ecd611>
2010-03-07 23:56:12 +0000

    implementing ectune's ISR.

---
+++ fetchISR.h
@@ -0,0 +1,73 @@
+short ISRSizeMain = 38;
+short ISRSizeSec = 15;
+short ISRCacheMain[38] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+short ISRCacheSec[15] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+unsigned short ISRChecksum = 0;
+
+void fetchISRMain {
+  
+  	if ( !bWaitingForAnswer )
+	{
+          if (millis() - LastPoll > SlowPollInterval) {
+            LastPoll = millis();   
+  
+            Serial1.flush();
+            Serial1.write(0x20);
+            bWaitingForAnswer = true;
+            LastCommand = millis();
+            CurReq = 0x20;
+          }
+        }
+  
+        if ( Serial1.available() >= ISRSizeMain && CurReq == 0x20)
+        {  
+            for ( short i = 0; i < ISRSizeMain-2; i++) {
+            ISRCacheMain[i]  = Serial1.read();
+            }
+              
+        ISRChecksum = Serial1.read();
+	bWaitingForAnswer = false;
+        Serial1.flush();
+              
+        }
+}
+
+void fetchISRSec {
+  
+  	if ( !bWaitingForAnswer )
+	{
+          if (millis() - LastPoll > SlowPollInterval) {
+            LastPoll = millis();   
+  
+            Serial1.flush();
+            Serial1.write(0x30);
+            bWaitingForAnswer = true;
+            LastCommand = millis();
+            CurReq = 0x30;
+          }
+        }
+  
+        if ( Serial1.available() >= ISRSizeSec && CurReq == 0x30)
+        {  
+            for ( short i = 0; i < ISRSizeSec-2; i++) {
+            ISRCacheSec[i]  = Serial1.read();
+            }
+              
+        ISRChecksum = Serial1.read();
+	bWaitingForAnswer = false;
+        Serial1.flush();
+              
+        }
+  
+}
+
+
+void processISRMain {
+  
+}
+
+
+void processISRSec {
+  
+}
+
